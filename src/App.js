53abb036762e3e603d214326116d9c5a6a114d07
felipeffranco <felipeffranco@outlook.com>
2022-11-09 20:57:26 -0300

    done

--- src/App.js
+++ src/App.js
@@ -42,29 +42,27 @@ function App() {
           </a> 
         </section>
       </header>
-      <article>
-        <ul>
-          <li>
-            <img src={icon1} className="icons" alt="icons" />
-            <h2>Declarative</h2>
-            <p>React makes it painless to create interactive UIs.</p>
-          </li>
-          <li>
-            <img src={icon2} className="icons" alt="icons" />
-            <h2>Declarative</h2>
-            <p>React makes it painless to create interactive UIs.</p>
-          </li>
-          <li>
-            <img src={icon3} className="icons" alt="icons" />
-            <h2>Declarative</h2>
-            <p>React makes it painless to create interactive UIs.</p>
-          </li>
-          <li>
-            <img src={icon4} className="icons" alt="icons" />
-            <h2>Declarative</h2>
-            <p>React makes it painless to create interactive UIs.</p>
-          </li>
-        </ul>
+      <article className="articles">
+        <article>
+          <img src={icon1} className="icons" alt="icons" />
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </article>
+        <article>
+          <img src={icon2} className="icons" alt="icons" />
+          <h2>Components</h2>
+          <p>Build encapsulated components that manage their state.</p>
+        </article>
+        <article>
+          <img src={icon3} className="icons" alt="icons" />
+          <h2>Single-Way</h2>
+          <p>A set of immutable values are passed to the components.</p>
+        </article>
+        <article>
+          <img src={icon4} className="icons" alt="icons" />
+          <h2>JSX</h2>
+          <p>Statically-typed, designed to run on modern browsers.</p>
+        </article>
       </article>
     </div>
   );
